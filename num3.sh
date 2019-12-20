9de68928e8b9e50b96a7d0db528a48e8efc9485d
Matan Wiesner <matanwiesner@matanwiesner-macbookpro.roam.corp.google.com>
2019-12-20 09:31:54 +0200

    edit some scripts

--- num3.sh
+++ num3.sh
@@ -1,1 +1,1 @@
-i=0; while [ $i -le 8 ]; do echo "num $1 $i"; sleep 1; i=$((i+1)); done
+i=0; while [ $i -le 7 ]; do echo "num $1 $i"; sleep 1; i=$((i+1)); done
--- read3.sh
+++ read3.sh
@@ -1,2 +1,2 @@
-i=0; while [ $i -le 8 ]; do read avar; echo "got $avar"; i=$((i+1)); done
-i=0; while [ $i -le 100 ]; do echo "read3 $1 $i"; sleep 1; i=$((i+1)); done
+i=0; while [ $i -le 7 ]; do read avar; echo "got $avar"; i=$((i+1)); done
+i=0; while [ $i -le 7 ]; do echo "read3 $1 $i"; sleep 1; i=$((i+1)); done
